4c0e2718cfcb54d94e94941d60aca2fd1871459d
pathum177 <pathummalsara177@gmail.com>
2025-05-28 23:29:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ global.mess = {
 global.owner = [process.env.OWNER_NUMBER] || ['94711453097'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'youre session id',
+SESSION_ID: process.env.SESSION_ID || 'PRABATH-MD~Iu0zlDAJ#4Zrk65uS9L5sUPisOghyrBF8zOqDePtBI2favo1jU20',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
 LANG:  process.env.LANG || 'SI' ,    
